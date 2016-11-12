1ff81f24d7426cb573f0a8db7de3279f8b94f287
Ianau Andrei <satyandrei@gmail.com>
2016-11-12 11:42:25 +0200

    -exception case: more marks in editTeza -hide keyboard onMedieClick -toggle(show) keyboard onClearClick -added hint in editNote, editTeza -changed the background color of editNote, editTeza -changed the text color of btnMedie, btnClear

--- app/src/main/java/com/example/alex/calculmedie/MainActivity.java
+++ app/src/main/java/com/example/alex/calculmedie/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.alex.calculmedie;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.EditText;
@@ -13,7 +16,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,37 +23,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /*private String clearSpace(String t){
-        char[] s;
-        s = t.toCharArray();
-        int len = s.length;
-        for(int i = 0; i < len; ++i){
-            if(s[i] == ' '){
-                int j = i + 1;
-                while(s[j] == ' ') {
-                    for(int p = j; p < len-1; ++p)
-                        s[p] = s[p+1];
-                    len--;
-                }
-            }
-        }
-        String l = s.toString();
-        return l;
-    }*/
-
     public void onClickMedie(View v){
-        String s;
-        String[] note;
-        s = ((EditText)findViewById(R.id.editNote)).getText().toString();
-        note = s.split("\\s");
-        s = ((EditText)findViewById(R.id.editTeza)).getText().toString();
+        //hide keyboard
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(findViewById(R.id.btnMedie).getWindowToken(), InputMethodManager.RESULT_UNCHANGED_SHOWN);
+
+        String sNote; // stringul pt note
+        String[] note; // notele introduse in editNote
+        sNote = ((EditText)findViewById(R.id.editNote)).getText().toString(); // gets the marks from the editNote
+        if (sNote.compareTo("") == 0) Toast.makeText(getApplicationContext(), "Introduceti note", Toast.LENGTH_LONG).show();
+        note = sNote.split("\\s"); // splits them in note[]
+
+        String sTeza; // stringul pt teza
+        sTeza = ((EditText)findViewById(R.id.editTeza)).getText().toString(); // gets the marks from editTeza
+        String[] teza;
+        teza = sTeza.split("\\s"); // puts all the marks from editTeza in teza[]
+
         double S = 0;
         int K = 0;
 
         try{
             for(String str : note){
-                    //String f = clearSpace(str);
-                    //System.out.println(f);
                     double nr;
                     if (str.compareTo("") != 0) {
                         nr = Integer.parseInt(str);
@@ -62,14 +54,15 @@ public class MainActivity extends AppCompatActivity {
             S/=K;
 
             try{
-                double teza = Integer.parseInt(s);
-                S = (S * 3 + teza)/4;
+                double Teza = Integer.parseInt(teza[0]); // parses the first mark in editTeza
+                S = (S * 3 + Teza)/4;
             }catch (Exception e){
                 Toast t =  Toast.makeText(getApplicationContext(), "Lipsa teza", Toast.LENGTH_SHORT);
                 t.show();
             }
 
-            ((TextView)findViewById(R.id.txtViewMedie)).setText(String.format("%.2f", S));
+            if (sNote.compareTo("") != 0) ((TextView)findViewById(R.id.txtViewMedie)).setText(String.format("%.2f", S));  // if the editTexts are filled do make the medie
+            else ((TextView)findViewById(R.id.txtViewMedie)).setText(""); // else show Medie as ""
 
         }catch(Exception e){
             Toast t = Toast.makeText(getApplicationContext(), "Date incorecte", Toast.LENGTH_SHORT);
@@ -79,6 +72,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickClear(View v){
+        //hide keyboard
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0); // aici nu am gasit cum sa fie doar "show keyboard".. :/ am gasit doar toggle
+        /*asta nu merge
+        imm.showSoftInput(findViewById(R.id.btnClear), InputMethod.SHOW_FORCED); */
+
         ((TextView)findViewById(R.id.txtViewMedie)).setText("");
         ((EditText)findViewById(R.id.editNote)).setText("");
         ((EditText)findViewById(R.id.editTeza)).setText("");
